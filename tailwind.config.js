399c72b762561cf38efa7a7e91b7dc8cebdf24b1
Ri087 <ri087@MacBook-Air-de-Ri087.local>
2025-02-04 23:09:32 +0100

    feat(burger): config menu burger

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'green-primary': 'var(--green-primary)',
+        'green-secondary': 'var(--green-secondary)',
+      },
+    },
+  },
+}
